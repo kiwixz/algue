36007df750ca26d1f50d10c8ba882f5c114799f7
kiwixz <kiwixz@outlook.com>
2020-10-12 02:40:50 +0200

    bump kae

--- cmake/kae.cmake
+++ cmake/kae.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 
 FetchContent_Declare("kae"
     GIT_REPOSITORY "https://github.com/kiwixz/kae"
-    GIT_TAG "351a71bb6b8f97dff8c6763feae09a312e18bcfd"
+    GIT_TAG "139a4f8fc35f42f8bbd980002ff4e22545b67902"
 )
 
 FetchContent_GetProperties("kae")
