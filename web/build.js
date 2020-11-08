8c6e15bead39b8ed76651f3d6e6203cd5fdd6919
kiwixz <kiwixz@outlook.com>
2020-11-09 00:34:12 +0100

    ignore eslint warning

--- web/build.js
+++ web/build.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const path = require("path");
+const path = require("path"); // eslint-disable-line import/no-extraneous-dependencies
 const { execSync } = require("child_process");
 
 execSync(path.join("riot", "make.py"), { stdio: "inherit" });
