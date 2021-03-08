06cad18f7f8cec7e48e2dd76c82a81413bb06446
kiwixz <kiwixz@outlook.com>
2021-03-08 15:56:21 +0100

    format web in format_all script

--- format_all.py
+++ format_all.py
@@ -8,13 +8,11 @@ import subprocess
 CLANG_FORMAT = os.getenv("CLANG_FORMAT", "clang-format")
 
 
-def format_file(path):
+def format_cpp_file(path):
     subprocess.check_call([CLANG_FORMAT, "-i", path])
 
 
-def main():
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
+def format_cpp():
     git_files = subprocess.check_output(["git", "ls-files"], text=True).splitlines()
     todo_files = [
         f
@@ -22,8 +20,21 @@ def main():
         if (f.endswith(".cpp") or f.endswith(".h")) and not f.startswith("cmake/") and os.path.exists(f)
     ]
     with multiprocessing.Pool(min(len(todo_files), os.cpu_count() + 2)) as p:
-        p.map(format_file, todo_files)
-    print(f"formatted {len(todo_files)} files")
+        p.map(format_cpp_file, todo_files)
+    print(f"formatted {len(todo_files)} cpp files")
+
+
+def format_web():
+    subprocess.check_call(["npx", "prettier", "-w", "."], cwd="web", stdout=subprocess.DEVNULL, shell=True)
+    subprocess.check_call(["npx", "eslint", "--fix", "."], cwd="web", stdout=subprocess.DEVNULL, shell=True)
+    print("formatted web")
+
+
+def main():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    format_cpp()
+    format_web()
 
 
 if __name__ == "__main__":
