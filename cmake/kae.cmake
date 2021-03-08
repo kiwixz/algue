c2de84fd188c4b6c17508ab18dc56c00c604e6c0
kiwixz <kiwixz@outlook.com>
2021-03-08 15:36:30 +0100

    commit clang tooling related files instead of linking them from kae

--- cmake/kae.cmake
+++ cmake/kae.cmake
@@ -9,31 +9,17 @@ FetchContent_Declare("kae"
 FetchContent_GetProperties("kae")
 if (kae_POPULATED)
     message(STATUS "third party 'kae' already populated, will not add this one")
-    set(add_this_kae OFF)
 else ()
-    set(add_this_kae ON)
     message(STATUS "fetching kae")
     FetchContent_Populate("kae")
-endif ()
 
-
-list(APPEND CMAKE_MODULE_PATH "${kae_SOURCE_DIR}/cmake")
-
-
-function (add_kae)
-    set(files_to_import
-        ".clang-format"
-        ".clang-tidy"
-        "format_all.py"
-    )
-    foreach (file ${files_to_import})
-        file(CREATE_LINK "${kae_SOURCE_DIR}/${file}" "${PROJECT_SOURCE_DIR}/${file}")
-    endforeach ()
-
-    if (add_this_kae)
+    function (add_kae)
         message(STATUS "adding kae")
         list(APPEND CMAKE_MESSAGE_CONTEXT "kae")
         add_subdirectory("${kae_SOURCE_DIR}" "${kae_BINARY_DIR}")
         list(POP_BACK CMAKE_MESSAGE_CONTEXT)
-    endif ()
-endfunction ()
+    endfunction ()
+endif ()
+
+
+list(APPEND CMAKE_MODULE_PATH "${kae_SOURCE_DIR}/cmake")
